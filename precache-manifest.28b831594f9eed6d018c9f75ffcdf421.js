ca53addd603dd5519d590d7bcd58db2eadda9f58
Ignacy <8687205+lopogo59@users.noreply.github.com>
2019-03-03 17:19:55 +0100

    Updates

---
+++ precache-manifest.28b831594f9eed6d018c9f75ffcdf421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c947ae507224ded5cd",
+    "url": "/image_to_excel_converter/static/js/runtime~main.75c947ae.js"
+  },
+  {
+    "revision": "30384ee927a7638cd54f",
+    "url": "/image_to_excel_converter/static/js/main.30384ee9.chunk.js"
+  },
+  {
+    "revision": "6e1768f00dc5acafc865",
+    "url": "/image_to_excel_converter/static/js/2.6e1768f0.chunk.js"
+  },
+  {
+    "revision": "30384ee927a7638cd54f",
+    "url": "/image_to_excel_converter/static/css/main.5d8c9810.chunk.css"
+  },
+  {
+    "revision": "a73e424315b572441e0355ca777c71ca",
+    "url": "/image_to_excel_converter/index.html"
+  }
+];
