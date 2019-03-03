d616e005be5134c3be90862ff385bdde58385af4
Ignacy <8687205+lopogo59@users.noreply.github.com>
2019-03-03 16:06:20 +0100

    Github Pages release

---
+++ precache-manifest.a00449e304819dab4af887e652a689c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "140712bfa852717c68de",
+    "url": "/static/js/main.140712bf.chunk.js"
+  },
+  {
+    "revision": "8d84961fac85ae1258c3",
+    "url": "/static/js/2.8d84961f.chunk.js"
+  },
+  {
+    "revision": "140712bfa852717c68de",
+    "url": "/static/css/main.bc7ccb80.chunk.css"
+  },
+  {
+    "revision": "7e7859fe5234151671f440fdbcc4b52f",
+    "url": "/index.html"
+  }
+];
